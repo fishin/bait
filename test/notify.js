342e3a416be8378bbb2102bd9afa459d55739289
lloydbenson <lloyd.benson@gmail.com>
2015-05-06 19:23:30 -0500

    handle array of statuses you want to get alerted on

--- test/notify.js
+++ test/notify.js
@@ -30,7 +30,11 @@ describe('notify', function () {
                 type: 'email',
                 to: 'lloyd.benson@gmail.com',
                 subject: 'subject',
-                message: 'message'
+                message: 'message',
+                statuses: [
+                    'failed',
+                    'fixed'
+                ]
             }
         };
         var bait = new Bait(internals.defaults);
@@ -78,7 +82,140 @@ describe('notify', function () {
         }, 1000);
     });
 
+    it('updateJob no status match', function (done) {
+
+        var config = {
+            notify: {
+                statuses: [
+                   'succeeded'
+                ]
+            }
+        };
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        var updateJob = bait.updateJob(jobId, config);
+        expect(updateJob.id).to.exist();
+        expect(updateJob.updateTime).to.exist();
+        expect(updateJob.notify.statuses[0]).to.equal('succeeded');
+        done();
+    });
+
+    it('startJob', function (done) {
+
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        bait.startJob(jobId, null);
+        var job = bait.getJob(jobId);
+        var runs = bait.getRuns(jobId, null);
+        var runId = runs[0].id;
+        var run = bait.getRun(jobId, null, runId);
+        expect(run.id).to.exist();
+        expect(run.startTime).to.exist();
+        expect(runs.length).to.equal(2);
+        done();
+    });
+
+    it('getRun', function (done) {
+
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        var runs = bait.getRuns(jobId, null);
+        var runId = runs[0].id;
+        var intervalObj2 = setInterval(function () {
+
+            var run = bait.getRun(jobId, null, runId);
+            //console.log(run);
+            if (run.finishTime) {
+                clearInterval(intervalObj2);
+                //console.log(run);
+                expect(run.status).to.equal('failed');
+                expect(run.id).to.exist();
+                expect(run.commands).to.be.length(1);
+                expect(run.commands[0].stdout).to.exist();
+                done();
+            }
+        }, 1000);
+    });
+
     it('deleteJob', function (done) {
+
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        bait.deleteJob(jobId);
+        jobs = bait.getJobs();
+        expect(jobs.length).to.equal(0);
+        done();
+    });
+
+    it('createJob nonotify', function (done) {
+
+        // switching this to pail later
+        var config = {
+            name: 'nonotify',
+            body: [
+                'invalid'
+            ],
+            notify: {
+                type: 'none',
+                to: 'lloyd.benson@gmail.com',
+                subject: 'subject',
+                message: 'message',
+                statuses: [
+                    'failed',
+                    'fixed'
+                ]
+            }
+        };
+        var bait = new Bait(internals.defaults);
+        var createJob = bait.createJob(config);
+        expect(createJob.id).to.exist();
+        done();
+    });
+
+    it('startJob nonotify', function (done) {
+
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        bait.startJob(jobId, null);
+        var job = bait.getJob(jobId);
+        var runs = bait.getRuns(jobId, null);
+        var runId = runs[0].id;
+        var run = bait.getRun(jobId, null, runId);
+        expect(run.id).to.exist();
+        expect(run.startTime).to.exist();
+        expect(runs.length).to.equal(1);
+        done();
+    });
+
+    it('getRun nonotify', function (done) {
+
+        var bait = new Bait(internals.defaults);
+        var jobs = bait.getJobs();
+        var jobId = jobs[0].id;
+        var runs = bait.getRuns(jobId, null);
+        var runId = runs[0].id;
+        var intervalObj2 = setInterval(function () {
+
+            var run = bait.getRun(jobId, null, runId);
+            //console.log(run);
+            if (run.finishTime) {
+                clearInterval(intervalObj2);
+                //console.log(run);
+                expect(run.status).to.equal('failed');
+                expect(run.id).to.exist();
+                expect(run.commands).to.be.length(1);
+                expect(run.commands[0].stdout).to.exist();
+                done();
+            }
+        }, 1000);
+    });
+
+    it('deleteJob nonotify', function (done) {
 
         var bait = new Bait(internals.defaults);
         var jobs = bait.getJobs();
